358ab17efae664a071a9a7e94a97044e0ef57009
lew <139905304+levrx@users.noreply.github.com>
2024-05-06 14:17:12 +0100

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -19,7 +19,7 @@ const CONFIG = {
         sortBy: 'stars', // Sort projects by 'stars' or 'updated'
         limit: 8, // How many projects to display.
         exclude: {
-          forks: false, // Forked projects will not be displayed if set to true.
+          forks: true, // Forked projects will not be displayed if set to true.
           projects: ['levrx/levrx.github.io' , 'levrx/levrx/levrx-proxy' , 'levrx/movie-vault'], // These projects will not be displayed. example: ['arifszn/my-project1', 'arifszn/my-project2']
         },
       },
